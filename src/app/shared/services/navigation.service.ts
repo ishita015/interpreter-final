bef99f691773313b372ef14c874999063181680b
gokul-samosys <gokul.rathod@samosys.com>
2020-12-01 12:23:45 +0530

    update code

--- src/app/shared/services/navigation.service.ts
+++ src/app/shared/services/navigation.service.ts
@@ -71,7 +71,7 @@ export class NavigationService {
             type: 'dropDown',
             icon: 'i-Library',
             sub: [
-                { icon: 'i-Bell', name: 'Language-List', state: '/languages/list', type: 'link' },
+                { icon: 'i-Bell', name: 'Language List', state: '/languages/list', type: 'link' },
                 { icon: 'i-Split-Horizontal-2-Window', name: 'Import Language', state: '/languages/excelImport', type: 'link' },
             ]
         },
