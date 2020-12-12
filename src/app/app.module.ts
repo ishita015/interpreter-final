f1c8f2bc8aec940c6c0dc690e01d557c0c58e1fb
gokul-samosys <gokul.rathod@samosys.com>
2020-12-12 18:49:56 +0530

    resolve conflict

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,9 @@ import { InMemoryDataService } from './shared/inmemory-db/inmemory-db.service';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
-const config: SocketIoConfig = { url: 'http://192.168.0.69:3300', options: {} };
+const config: SocketIoConfig = { url: 'http://192.168.0.4:3300', options: {} };
+
+//interpreter Run at http://192.168.0.4:3300
 
 
  
