e19a7e445a0bcefeb6c37c3e7b775dee115d4d9d
ishita <ishita.pardeshi@samosys.com>
2020-12-18 15:43:32 +0530

    interpreter-history

--- src/app/views/interpreter-request/interpreter-history-list/interpreter-history-list.component.ts
+++ src/app/views/interpreter-request/interpreter-history-list/interpreter-history-list.component.ts
@@ -28,54 +28,50 @@ export class InterpreterHistoryListComponent implements OnInit {
 
   ngOnInit(){
     this.requestId = JSON.parse(localStorage.getItem('interhistory'));
-    // this.searchControl.valueChanges
-    // .pipe(debounceTime(200))
-    // .subscribe(value => {
-    //   this.filerData(value);
-    // });
+    this.searchControl.valueChanges
+    .pipe(debounceTime(200))
+    .subscribe(value => {
+      this.filerData(value);
+    });
     this.numOfInterpreter();
   }
 
-  // filerData(val) {
-  //   if (val) {
-  //     val = val.toLowerCase();
-  //   } else {
-  //     console.log("xxxxxxx", this.filteredUser);
-  //     return this.filteredUser = [... this.userData];
-  //   }
+  filerData(val) {
+    if (val) {
+      val = val.toLowerCase();
+    } else {
+      console.log("xxxxxxx", this.filteredUser);
+      return this.filteredUser = [... this.userData];
+    }
 
-  //   const columns = Object.keys(this.userData[0]);
-  //   if (!columns.length) {
-  //     return;
-  //   }
+    const columns = Object.keys(this.userData[0]);
+    if (!columns.length) {
+      return;
+    }
 
-  //   const rows = this.userData.filter(function (d) {
-  //     for (let i = 0; i <= columns.length; i++) {
-  //       const column = columns[i];
-  //       // console.log(d[column]);
-  //       if (d[column] && d[column].toString().toLowerCase().indexOf(val) > -1) {
-  //         return true;
-  //       }
-  //     }
-  //   });
-  //   this.filteredUser = rows;
-  // }
+    const rows = this.userData.filter(function (d) {
+      for (let i = 0; i <= columns.length; i++) {
+        const column = columns[i];
+        // console.log(d[column]);
+        if (d[column] && d[column].toString().toLowerCase().indexOf(val) > -1) {
+          return true;
+        }
+      }
+    });
+    this.filteredUser = rows;
+  }
 
   numOfInterpreter(){
     console.log(this.requestId);
-    
     this.service.getNoOfInterpreter(this.requestId).subscribe(res => {
       if(res['status'] == 1){
-        this.interpreter_obj = res['data'][0];
+        this.interpreter_obj = res['data'];
         this.userData = [...res['data']];
-        // this.filteredUser = this.interpreter_obj;
-        // console.log("view object",  this.interpreter_obj);
-    
+        this.filteredUser = this.interpreter_obj;
       }else{
         this.resp_msg = res;
         this.toastr.error(this.resp_msg.message,'', { timeOut: 2000 });
       }
-        
     })
   }
 
