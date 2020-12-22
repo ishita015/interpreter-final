5a841b636afa24b41bf96359730ee44e9f14476f
gokul-samosys <gokul.rathod@samosys.com>
2020-12-22 17:36:32 +0530

    update skill code

---
+++ src/app/shared/models/interpreter-skills.ts
@@ -0,0 +1,7 @@
+export interface InterpreterSkills {
+    primary_language: any,
+    secondary_language: any,
+    interpreter_id: any,
+    documents: any,
+    other: any,
+}
--- src/app/views/interpreter-management/interpreter-profile/interpreter-profile.component.ts
+++ src/app/views/interpreter-management/interpreter-profile/interpreter-profile.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from "../../../shared/services/http.service";
 import { ValidationsService } from 'src/app/shared/services/validations.service';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 @Component({
@@ -10,7 +10,20 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./interpreter-profile.component.scss']
 })
 export class InterpreterProfileComponent implements OnInit {
+  items = ['Javascript', 'Typescript']
   bankingForm: FormGroup;
+  userForm: FormGroup;
+  langForm: FormGroup;
+  communityForm: FormGroup;
+  conferenceForm: FormGroup;
+  courtForm: FormGroup;
+  credentialedForm: FormGroup;
+  equipmentForm: FormGroup;
+  legalForm: FormGroup;
+  simultaneousForm: FormGroup;
+  otherForm: FormGroup;
+  tagsCtrl1 = new FormControl(this.items);
+  tagsCtrl2 = new FormControl([]);
   submitted: boolean;
   banking_Obj;
   banking_Msg;
@@ -22,7 +35,21 @@ export class InterpreterProfileComponent implements OnInit {
   check_form3 = false;
   check_form4 = false;
   check_form5 = false;
-  
+
+  communityinter:boolean = false;
+  conferenceinter:boolean = false;
+  courtcertified:boolean = false;
+  court_open:boolean = false;
+  credent_open:boolean = false;
+  equipment_open:boolean = false;
+  legal_open:boolean = false;
+  simult_open:boolean = false;
+  other_open:boolean = false;
+
+
+  public priLanguageId;
+  language_Obj;
+
   constructor(public validation: ValidationsService,
     private fb: FormBuilder,
     private toastr: ToastrService,
@@ -32,6 +59,22 @@ export class InterpreterProfileComponent implements OnInit {
   ngOnInit(){
     this.createForm();
     this.createForm2();
+
+    this.commForm();
+    this.languageForm();
+
+
+    this.conForm();
+    this.couForm();
+    this.creForm();
+    this.eqpForm();
+    this.legForm();
+    this.simForm();
+
+
+    this.othForm();
+    
+    this.LanguageList();
   }
 
    /*========== Form Value Start Here========*/
@@ -78,24 +121,24 @@ export class InterpreterProfileComponent implements OnInit {
       return;
     }
     this.submitted = false;
-    this.service.getInterpreterProfileAdd(this.Profile.value)
-    .subscribe(res => {
-      if(res['status']== 1){
-        console.log("api response",res);
-        this.ass_Obj = res
-        this.ass_Msg = res
-        this.toastr.success(this.ass_Msg.message,'', { timeOut: 1000 });
-        // this.router.navigate(['/languages/list']);  
-      }else{
-        console.log("api response",res);
-        this.ass_Obj = res
-        this.ass_Msg = res
-        this.toastr.success(this.ass_Msg.message,'', { timeOut: 1000 });
-        // this.router.navigate(['/login'])
-        // this.router.navigate(['/languages/list']);  
-      }
+    // this.service.getInterpreterProfileAdd(this.Profile.value)
+    // .subscribe(res => {
+    //   if(res['status']== 1){
+    //     console.log("api response",res);
+    //     this.ass_Obj = res
+    //     this.ass_Msg = res
+    //     this.toastr.success(this.ass_Msg.message,'', { timeOut: 1000 });
+    //     // this.router.navigate(['/languages/list']);  
+    //   }else{
+    //     console.log("api response",res);
+    //     this.ass_Obj = res
+    //     this.ass_Msg = res
+    //     this.toastr.success(this.ass_Msg.message,'', { timeOut: 1000 });
+    //     // this.router.navigate(['/login'])
+    //     // this.router.navigate(['/languages/list']);  
+    //   }
 
-    });
+    // });
   }
 
   /*==========assignment setting Form2 end Here========*/
@@ -168,5 +211,214 @@ export class InterpreterProfileComponent implements OnInit {
     this.check_form4 = false;
     this.check_form5 = true;
   }
+
+
+
+  //gokul code start  -------interpreter skills---------
+  //get language list
+  LanguageList() {
+    this.service.getLanguageList().subscribe(res => {
+        if(res['status']=='1'){
+          this.language_Obj = res['data'];
+          console.log("my testing", this.language_Obj);
+        }
+    });
+  }
+
+
+  //get primary language id
+  onChange(id) {
+    this.priLanguageId = id.target.value;
+    console.log("pri-lang", this.priLanguageId);
+  }
+
+  onSelect(item) {
+      console.log('tag selected: value is' + item);
+  }
+
+  communityShow(){
+    this.communityinter=true;  
+  }
+  conferenceShow(){
+    this.conferenceinter=true;  
+  }
+  certifiedShow(){
+    this.court_open=true;  
+  }
+  courtShow(){
+    this.credent_open=true;  
+  }
+  qualifiedShow(){
+    this.equipment_open=true;  
+  }
+  legalShow(){
+    this.legal_open=true;  
+  }
+  simultShow(){
+    this.simult_open=true;  
+  }
+  othShow(){
+    this.other_open=true;  
+  }
+
+
+
+  setCheck(event,eve_key){
+    // console.log("yes", eve_key);
+    if(eve_key=='1'){
+      if ( event.target.checked ) {
+        this.communityinter=true;
+      }else{
+        this.communityinter=false;
+      }
+    }
+
+    if(eve_key=='2'){
+      if ( event.target.checked ) {
+        this.conferenceinter=true;
+      }else{
+        this.conferenceinter=false;
+      }
+    }
+
+
+    if(eve_key=='3'){
+      if ( event.target.checked ) {
+        this.court_open=true;
+      }else{
+        this.court_open=false;
+      }
+    }
+
+
+    if(eve_key=='4'){
+      if ( event.target.checked ) {
+        this.credent_open=true;
+      }else{
+        this.credent_open=false;
+      }
+    }
+
+    if(eve_key=='5'){
+      if ( event.target.checked ) {
+        this.equipment_open=true;
+      }else{
+        this.equipment_open=false;
+      }
+    }
+
+    if(eve_key=='6'){
+      if ( event.target.checked ) {
+        this.legal_open=true;
+      }else{
+        this.legal_open=false;
+      }
+    }
+
+    if(eve_key=='7'){
+      if ( event.target.checked ) {
+        this.simult_open=true;
+      }else{
+        this.simult_open=false;
+      }
+    }
+
+    if(eve_key=='8'){
+      if ( event.target.checked ) {
+        this.other_open=true;
+      }else{
+        this.other_open=false;
+      }
+    }
+  }
+
+  
+  
+  
+  
+  languageForm() {
+    this.langForm = this.fb.group({
+      interpreter_id: [''],
+      primary_language: [''],
+      secondary_language: [''],      
+    });
+  }
+
+
+
+  commForm() {
+    this.communityForm = this.fb.group({
+      interpreter_id: [''],
+      documents: [''],
+    });
+  }
+
+
+  
+  conForm() {
+    this.conferenceForm = this.fb.group({
+      interpreter_id: [''],
+      documents: [''],
+    });
+  }
+
+
+
+  couForm() {
+    this.courtForm = this.fb.group({
+      interpreter_id: [''],
+      documents: [''],
+    });
+  }
+
+
+
+  creForm() {
+    this.credentialedForm = this.fb.group({
+      interpreter_id: [''],
+      documents: [''],
+    });
+  }
+
+
+
+  eqpForm() {
+    this.equipmentForm = this.fb.group({
+      interpreter_id: [''],
+      documents: [''],
+    });
+  }
+
+
+
+  legForm() {
+    this.legalForm = this.fb.group({
+      interpreter_id: [''],
+      documents: [''],
+    });
+  }
+
+
+
+  simForm() {
+    this.simultaneousForm = this.fb.group({
+      interpreter_id: [''],
+      documents: [''],
+    });
+  }
+
+
+  othForm() {
+    this.otherForm = this.fb.group({
+      interpreter_id: [''],
+      documents: [''],
+      other: [''],      
+    });
+  }
+
+
+
+
+  //gokul code end
 
 }
