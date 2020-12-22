6c9c8b175ed03865dd1bbba4dc7d705b179f3c14
ishita <ishita.pardeshi@samosys.com>
2020-12-22 15:43:14 +0530

    profile-detail

---
+++ src/app/shared/models/add-banking.ts
@@ -0,0 +1,12 @@
+export interface AddBanking {
+    account_no: any,
+    country:any, 
+    financial_institution:any,
+    payment_benificiary:any,
+    payment_method:any,
+    routing_number:any,
+    SWIFT_code:any,
+    fusion_id:any,
+    site_code:any,
+    site_id:any,
+}
