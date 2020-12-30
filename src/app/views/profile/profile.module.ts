1c10acafb2aef49553fabb14c9e501758e1499ef
ishita <ishita.pardeshi@samosys.com>
2020-12-30 18:47:20 +0530

    mobile number format change

--- src/app/views/profile/profile.module.ts
+++ src/app/views/profile/profile.module.ts
@@ -5,13 +5,14 @@ import { ProfileRoutingModule } from './profile-routing.module';
 import { AdminProfileComponent } from './admin-profile/admin-profile.component';
 import { ChangePasswordComponent } from './change-password/change-password.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { NgxMaskModule } from 'ngx-mask';
 @NgModule({
   declarations: [AdminProfileComponent, ChangePasswordComponent],
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
+    NgxMaskModule.forRoot(),
     ProfileRoutingModule
   ]
 })
