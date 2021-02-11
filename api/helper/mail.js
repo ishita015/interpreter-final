8fff2e99927e628cfc8875934aafb84bbfb83f3e
lukesh-samosys <devnarayan.dhaosriya@samosys.com>
2021-02-11 18:59:09 +0530

    d

--- api/helper/mail.js
+++ api/helper/mail.js
@@ -14,6 +14,7 @@ let transporter = nodemailer.createTransport(smtp);
 var helper = new function () {
     var subject,html;
     this.forgetPasswordMail = async function (value, cb) {
+        console.log('url',value)
             subject =value.subject;
             html = value.body;
         transporter.sendMail({ from: smtp.auth.user, to: value.email, subject: subject, html: html })
