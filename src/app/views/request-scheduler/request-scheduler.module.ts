4cae944b3f135b660c2e1e6470f63bf89a3b2508
ishita <ishita.pardeshi@samosys.com>
2021-02-11 14:03:52 +0530

    changes in request scheduler form

--- src/app/views/request-scheduler/request-scheduler.module.ts
+++ src/app/views/request-scheduler/request-scheduler.module.ts
@@ -9,6 +9,10 @@ import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import { SharedComponentsModule } from 'src/app/shared/components/shared-components.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgxMaskModule } from 'ngx-mask';
+import { AgmCoreModule } from '@agm/core';
+import { TimePickerModule } from '@syncfusion/ej2-angular-calendars';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatInputModule} from '@angular/material/input';
 @NgModule({
   declarations: [AllRequestSchedularComponent, AllRequestListSchedularComponent],
   imports: [
@@ -19,6 +23,13 @@ import { NgxMaskModule } from 'ngx-mask';
     NgxDatatableModule,
     SharedComponentsModule,
     NgbModule,
+    TimePickerModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBC_D6nmnxc-Og6sZJ_hfuxRbHcJB9Is38',//'AIzaSyBkH1R6_PO7fNY5L0z6CvV00oeNX8Vn0iE', 
+      libraries: ['places']
+    }),
     NgxMaskModule.forRoot(),
   ]
 })
